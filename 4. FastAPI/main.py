9e295745624bb8bc87badd107ad600d3a1fef27d
Vishal-Nishad <vishal.n1020@gmail.com>
2026-03-23 23:28:14 +0530

    fastapi jsonable_encoder

--- 4. FastAPI/main.py
+++ 4. FastAPI/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
@@ -10,11 +11,19 @@ def greet():
     st = {"greet":"hello this is vishal"}
     return st
 
+products = [
+    Product(1,"phone", "budget phone", 99, 10),
+    Product(2,"laptop", "budget laptop", 500, 40),
+]
+
+print(products[0].__dict__)
+
+print(products)
+
 @app.get("/products")
 def get_all_products():
-    return "All products"
+    return products
 
-
-
+print(get_all_products())
 
 print(greet())
